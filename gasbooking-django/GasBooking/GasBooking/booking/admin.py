b2eb487102475f4d80d8daafefe5b7dee06d706c
Nikita Agarwal <nikinigania@gmail.com>
2020-07-15 23:50:56 +0530

    Add files via upload

---
+++ gasbooking-django/GasBooking/GasBooking/booking/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Registration)
+admin.site.register(Connection)
+admin.site.register(Price)
+admin.site.register(Booking)
+admin.site.register(Status)
+admin.site.register(SubOffice)
